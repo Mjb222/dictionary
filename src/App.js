57524cb83f677e78fa052dc5a28f3cfb346ed4eb
Mjb222 <94459374+Mjb222@users.noreply.github.com>
2022-05-19 12:14:19 +0200

    search engine update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      {" "}
-      hello
-      <header className="App-header"></header>
+      <div className="container">
+        hello
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Project coded by Marium Bajwa,</small>
+        </footer>
+      </div>
     </div>
   );
 }
